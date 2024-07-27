948bfadf81ce088a4f442d07d92fa2bffddc9d75
Fhulufhelo Shaun Bulasigobo <33252250+Fshaun@users.noreply.github.com>
2024-07-27 13:11:09 +0200

    MedChain

---
+++ Controllers/ReminderController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PatientRecordManagement.Controllers
+{
+    public class ReminderController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Models/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+public class ApplicationDbContext : IdentityDbContext<IdentityUser>
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+    }
+
+    // Define your DbSets here. For example:
+    // public DbSet<PatientRecord> PatientRecords { get; set; }
+}
